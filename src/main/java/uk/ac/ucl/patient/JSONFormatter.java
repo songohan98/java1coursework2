9d2f50ac3fa774a7dd84d59c75964ed76f07ac10
songohan98 <43612083+songohan98@users.noreply.github.com>
2019-03-09 21:05:22 +0000

    not sure

---
+++ src/main/java/uk/ac/ucl/patient/JSONFormatter.java
@@ -0,0 +1,30 @@
+package uk.ac.ucl.patient;
+
+public class JSONFormatter
+{
+	"Patients":
+            [
+	        {
+            "ID": "",
+            "BIRTHDATE": "",
+            "DEATHDATE": "",
+            "SSN": "",
+            "DRIVERS": "",
+            "PASSPORT": "",
+            "PREFIX": "",
+            "FIRST": "",
+            "LAST": "",
+            "SUFFIX": "",
+            "MAIDEN": "",
+            "MARITAL": "",
+            "RACE": "",
+            "ETHNICITY": "",
+            "GENDER": "",
+            "BIRTHPLACE": "",
+            "ADDRESS": "",
+            "CITY": "",
+            "STATE": "",
+            "ZIP": ""
+	        }
+	        ]
+}
